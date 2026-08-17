1b69cd0d80789710582785970a1aa2655276226d
Siva <sivafx@gmail.com>
2026-08-17 15:28:52 +0530

    Add files via upload

---
+++ ecommerce/app.js
@@ -0,0 +1,3 @@
+// The application is implemented in index.html.
+// This file is intentionally included as a placeholder so the project can
+// be split into index.html, styles.css and app.js for classroom exercises.
